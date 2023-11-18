e889ff9cd567832eac410e918cc8d7d4d9a1a422
Evgeniya Itkina <eva.last.ita@gmail.com>
2023-11-18 20:44:31 +0200

    ex3

--- ProjectDocument/document.js
+++ ProjectDocument/document.js
@@ -1,19 +1,21 @@
-/* let elementWidth = document.getElementById('width').value + 'px';
-let elementHeight = document.getElementById('height').value + 'px';
-let elementText = document.getElementById('write').value;
+/* let elementText = document.getElementById('write').value;
 
-let elementPlace = document.querySelector('.place').value; */
+let elementPlace = document.querySelector('.place').value;  */
 
 
 function doIt() {
 
     const elementType = document.getElementById("element").value;
     const elementColor = document.getElementById('color').value;
+    const elementWidth = document.getElementById('width').value + 'px';
+    const elementHeight = document.getElementById('height').value + 'px';
     console.log(elementType);
     console.log(elementColor);
+    console.log(elementWidth);
+    console.log(elementHeight);
     switch (elementType) {
         case "Div":
-            document.querySelector(".whiteSheet").innerHTML += `<div style='background-color:${elementColor}; width:100px; height:100px; border: 1px solid black '></div>`;
+            document.querySelector(".whiteSheet").innerHTML += `<div style='background-color:${elementColor}; width:${elementWidth}; height:${elementHeight}; border: 1px solid black '></div>`;
             break;
         case "Paragraph":
             document.querySelector(".whiteSheet").innerHTML += `<p style='color:${elementColor}; font-size: 1.2em'>Hello Word</p>`;
