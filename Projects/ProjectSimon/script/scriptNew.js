c629b9916e52a1872e3f8effb3b6fed87fcfd2a8
Evgeniya Itkina <eva.last.ita@gmail.com>
2024-02-04 22:08:59 +0200

    AddPicturesSimonGame

--- Projects/ProjectSimon/script/scriptNew.js
+++ Projects/ProjectSimon/script/scriptNew.js
@@ -1,74 +1,111 @@
-const colors = ["red", "green", "yellow", "blue"]
-const changeColor = ["lightcoral", "lightgreen", "lightyellow", "lightblue"]
-const music = [new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
-new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
-new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
-new Audio("https://www.fesliyanstudios.com/play-mp3/387")]
-let indexArrCompColor = 0
-let round = 0;
-let arrCompColors = [];
+document.getElementById('red').addEventListener('click', playerClick.bind(null, 0));
+document.getElementById('green').addEventListener('click', playerClick.bind(null, 1));
+document.getElementById('yellow').addEventListener('click', playerClick.bind(null, 2));
+document.getElementById('blue').addEventListener('click', playerClick.bind(null, 3));
+const gameOver = document.getElementById("gameOver")
+const message = document.getElementById("message")
 
+const gameColors = ["red", "green", "yellow", "blue"];
+const highlightColors = ["lightcoral", "lightgreen", "lightyellow", "lightblue"];
+const soundEffects = [
+    new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
+    new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
+    new Audio("https://www.fesliyanstudios.com/play-mp3/387"),
+    new Audio("https://www.fesliyanstudios.com/play-mp3/387")
+];
+
+const images = [
+    './image/gameOver1.jpg',
+    './image/gameOver2.jpg',
+    './image/gameOver3.jpg',
+    './image/gameOver4.jpg',
+    './image/gameOver5.jpg',
+    './image/gameOver6.jpg',
+    './image/gameOver7.jpg',
+    './image/gameOver8.jpg',
+    './image/gameOver9.jpg',
+    './image/gameOver10.jpg'
+];
+
+let currentSequenceIndex = 0;
+let currentRound = 0;
+let computerSequence = [];
 
 async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function computerTurn() {
+    currentSequenceIndex = 0;
+    currentRound++;
+    document.getElementById("score").innerHTML = `Round: ${currentRound}`;
+    let colorIndex = Math.floor(Math.random() * 4);
+    computerSequence.push(gameColors[colorIndex]);
 
-async function computerGame() {
-    indexArrCompColor = 0;
-    round++;
-    document.getElementById("score").innerHTML = round;
-    let color = Math.floor(Math.random() * 4)
-    arrCompColors.push(colors[color]);
-
-    console.log(arrCompColors);
-    for (let i = 0; i <= arrCompColors.length; i++) {
-        let currColor = arrCompColors[i]
-        let colorIndex = colors.indexOf(currColor);
-        music[colorIndex].play();
+    console.log(computerSequence);
+    for (let i = 0; i < computerSequence.length; i++) {
+        let currColor = computerSequence[i];
+        let colorIndex = gameColors.indexOf(currColor);
+        await sleep(500);
+        soundEffects[colorIndex].play();
         let el = document.getElementById(currColor);
-        el.style.backgroundColor = changeColor[colorIndex];
+        el.style.backgroundColor = highlightColors[colorIndex];
 
         await sleep(500);
-        el.style.backgroundColor = colors[colorIndex]
-        console.log(colors[colorIndex]);
+        el.style.backgroundColor = gameColors[colorIndex];
+        console.log(gameColors[colorIndex]);
     }
+    await sleep(1000);
 }
 
-document.getElementById('red').addEventListener('click', userClick.bind(null, 0))
-document.getElementById('green').addEventListener('click', userClick.bind(null, 1))
-document.getElementById('yellow').addEventListener('click', userClick.bind(null, 2))
-document.getElementById('blue').addEventListener('click', userClick.bind(null, 3))
-async function userClick(color) {
-    music[color].play();
-    let currColor = colors[color]
+async function playerClick(colorIndex) {
+    soundEffects[colorIndex].play();
+    let currColor = gameColors[colorIndex];
     let el = document.getElementById(currColor);
-    el.style.backgroundColor = changeColor[color];
+    el.style.backgroundColor = highlightColors[colorIndex];
 
     await sleep(500);
-    el.style.backgroundColor = colors[color]
-    if (colors[color] != arrCompColors[indexArrCompColor]) {
-        gameOver();
-        return
+    el.style.backgroundColor = gameColors[colorIndex];
+    if (gameColors[colorIndex] != computerSequence[currentSequenceIndex]) {
+        endGame();
+        return;
     }
 
-    if (indexArrCompColor == arrCompColors.length - 1) {
-
-        computerGame()
-        return
+    if (currentSequenceIndex == computerSequence.length - 1) {
+        computerTurn();
+        return;
     }
 
-    indexArrCompColor++
+    currentSequenceIndex++;
 }
 
+function endGame() {
+    gameOver.style.display = "flex";
 
-function gameOver() {
-    alert("Game Over")
-    startGame()
+    // Выбор случайного индекса из массива images
+    const randomIndex = Math.floor(Math.random() * images.length);
+
+    // Получение случайного URL изображения из массива
+    const randomImage = images[randomIndex];
+
+    // Установка случайного изображения для элемента img внутри div#gameOver
+    document.querySelector("#gameOver img").src = randomImage;
+
+    message.innerHTML = `Result is ${currentRound}. <br> Try again <br>`;
+
+    computerSequence = [];
+    currentRound = 0;
 }
-document.getElementById("btnStart").addEventListener('click', startGame)
-function startGame() {
-    arrCompColors = [];
-    round = 0;
-    computerGame()
+
+document.getElementById("btnStart").addEventListener('click', startNewGame);
+
+function startNewGame() {
+    gameOver.style.display = "none"
+    computerSequence = [];
+    currentRound = 0;
+    computerTurn();
 }
+
+document.getElementById("closeButton").addEventListener("click", function () {
+    gameOver.style.display = "none";
+});
